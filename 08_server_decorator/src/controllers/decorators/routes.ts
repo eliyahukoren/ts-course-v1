908512bc0ef4d0d36c20010ece59047844855797
eliyahukoren <elik.koren@gmail.com>
2022-11-30 18:31:49 +0200

    define a route binder

--- 08_server_decorator/src/controllers/decorators/routes.ts
+++ 08_server_decorator/src/controllers/decorators/routes.ts
@@ -1,13 +1,25 @@
 import "reflect-metadata";
 
-export function get(path: string){
-	// this is the decorator factory, it sets up
-	// the returned decorator function
-	return function(target: any, key: string, desc: PropertyDescriptor){
-		// this is the DECORATOR
-		// it will be applied for a method and
-		// path: function argument, key: method name 
-		console.log('get decorator:', path, key)
-		Reflect.defineMetadata('path', path, target, key)
+
+function routeBinder(method: string){
+	return function (path: string){
+		// this is the decorator factory, it sets up
+		// the returned decorator function
+		return function(target: any, key: string, desc: PropertyDescriptor){
+			// this is the DECORATOR
+			// it will be applied for a method and
+			// path: function argument, key: method name 
+			console.log('get decorator:', path, key)
+			Reflect.defineMetadata('path', path, target, key)
+			Reflect.defineMetadata('method', method, target, key)
+		}
 	}
 }
+
+export const get = routeBinder('get');
+export const put = routeBinder('put');
+export const post = routeBinder('post');
+export const del = routeBinder('delete');
+export const patch = routeBinder('patch');
+
+
