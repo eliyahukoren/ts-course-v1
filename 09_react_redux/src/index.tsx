5895dae701b7c248642fcc812dec0412a76c497c
eliyahukoren <elik.koren@gmail.com>
2022-12-01 12:42:31 +0200

    just for example, how to use initial state in the react class component with ts

--- 09_react_redux/src/index.tsx
+++ 09_react_redux/src/index.tsx
@@ -6,11 +6,55 @@ interface AppProps {
 	textSize?: number; // define optional props for Component
 }
 
-class App extends React.Component<AppProps> {
+interface AppState {
+	counter: number;
+}
+
+class App extends React.Component<AppProps, AppState> {
+
+	// possible definition of state is:
+	// then we no need to provide the second generic type
+	// state = {number: 0}
+
+	// other way, is use constructor and provide the generic type
+	constructor(props: AppProps){
+		super(props);
+
+		this.state = {counter: 0};
+	}
+
+	onIncrement = (): void => { 
+		this.setState({counter: this.state.counter + 1});
+	};
+	onDecrement = (): void => {
+		this.setState({ counter: this.state.counter - 1 });
+	};
+
 	render(): React.ReactNode {
-		return(
+		return (
 			<div className="container">
-				<h1 className="text-center">Hi There: {this.props.color}</h1>
+				<div className="card mt-5 mx-auto shadow-lg" style={{ width: "18rem" }}>
+					<div className="card-body">
+						<h5 className="card-title">Counter example</h5>
+						<div className="d-grid gap-2 col mx-auto mt-5">
+							<button
+								className="btn btn-primary"
+								onClick={this.onIncrement}
+							>
+								increment
+							</button>
+							<button
+								className="btn btn-success"
+								onClick={this.onDecrement}
+							>
+								decrement
+							</button>
+							<p className="text-center">
+								current counter: {this.state.counter}
+							</p>
+						</div>
+					</div>
+				</div>
 			</div>
 		);
 	}
