82202ee710b31f5d59c1e69368ee41b9e6ddd5ea
eliyahukoren <elik.koren@gmail.com>
2022-12-01 11:03:03 +0200

    define simple component and interface to provide generic type of props

--- 09_react_redux/src/index.tsx
+++ 09_react_redux/src/index.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-class App extends React.Component {
+interface AppProps {
+	color: string;
+}
+
+class App extends React.Component<AppProps> {
 	render(): React.ReactNode {
-		return <div>Hi There!</div>;
+		return(
+			<div className="container">
+				<h1 className="text-center">Hi There: {this.props.color}</h1>
+			</div>
+		);
 	}
 }
 
 ReactDOM.render(
-	<App />,
+	<App color="red" />,
 	document.querySelector('#root')
 );
